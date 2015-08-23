9debad10540c180e2ff5a5fe9e334106a2b61b19
Daniel Lamando <dan@danopia.net>
2015-08-23 15:35:55 -0700

    Start chroots on demand

--- index.js
+++ index.js
@@ -1,43 +1,53 @@
-document.querySelector('[icon=close]').onclick = function () {
-  window.close();
-};
+var closeButton = document.querySelector('[icon=close]');
+var newButton = document.querySelector('paper-fab');
+var chrootList = document.querySelector('dynamic-list');
+var chrootInfo = document.querySelector('chroot-info');
 
-document.querySelector('paper-fab').onclick = function () {
-  chrome.app.window.create(
-    'create-chroot.html',
-    {
-      id: 'createChroot',
-      frame: 'none',
-      bounds: {width: 700, height: 600}
-    }
-  );
+closeButton.onclick = function () { window.close(); };
+
+newButton.onclick = function () {
+  chrome.app.window.create('create-chroot.html', {
+    id: 'createChroot',
+    frame: 'none',
+    bounds: {width: 700, height: 600}
+  });
 };
 
 var chroots = {};
+var chrootArray = [];
 
+// get a chroot list at startup
 chrome.runtime.sendMessage({cmd: 'list chroots'}, function (response) {
-  document.querySelector('dynamic-list').items = response.chroots.map(function (chroot) {
-    chroots[chroot.key] = chroot;
-    
-    return {
+  chrootArray = response.chroots.map(function (chroot) {
+    return (chroots[chroot.key] = {
       key: chroot.key,
       label: chroot.key,
+      state: chroot.state,
       description: '(' + chroot.state + ')',
-    };
+    });
   });
+  
+  chrootList.notifyPath('items', chrootArray);
 });
 
+function changeState(chroot, state) {
+  chroot.state = state;
+  chroot.description = '(' + state + ')';
+
+  var descPath = 'items.' + chrootArray.indexOf(chroot) + '.description';
+  chrootList.notifyPath(descPath, chroot.description);
+}
+
 document.querySelector('dynamic-list').onselect = function (e, item) {
-  //console.log(item);
-  //if (!item.selected) return;
-  
   var chroot = chroots[this.selected];
-  document.querySelector('chroot-info').selectChroot(chroot);
-  
   if (chroot.state == 'stopped') {
-    chrome.runtime.sendMessage({cmd: 'start chroot', chroot: item.selected}, function (leaf) {
-      chroot.state = 'running';
-      //document.querySelector('chroot-info').chroot.port = leaf.port;
+    changeState(chroot, 'starting');
+    chrome.runtime.sendMessage({cmd: 'start chroot', chroot: chroot.key}, function (leaf) {
+      changeState(chroot, 'running');
+      chrootInfo.selectChroot(chroot);
     });
+    
+  } else {
+    chrootInfo.selectChroot(chroot);
   }
 };
